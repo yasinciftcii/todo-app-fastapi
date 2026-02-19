d378c6d4cc8b476cd20be1a5b792346e6d509e9a
yasinciftcii <yasinciftci_32@outlook.com>
2026-02-19 21:44:16 +0300

    feat/adding vercel domain

--- backend/main.py
+++ backend/main.py
@@ -30,6 +30,7 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:3000",
         "http://127.0.0.1:3000",
+        "https://dotodo-app.vercel.app"
     ],
     allow_credentials=True,
     allow_methods=["*"],
